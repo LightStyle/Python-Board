90517b6646635e3d4344fb1f16b9fccfd82b2cba
Niccolo' Campolungo <damnednickix@hotmail.it>
2010-08-14 12:51:17 +0200

    Riorganizzazione progetto

---
+++ src/index.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return 'Index page'
+
+@app.route("/topic/<id>")
+def topic(id):
+    return 'Topic con id {id}'.format(id=id)
+
+@app.route("/section/<id>")
+def section(id):
+    return 'Sezione con id {id}'.format(id=id)
+
+@app.route("/user/<id>")
+def user(id):
+    return 'Utente con id {id}'.format(id=id)
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
