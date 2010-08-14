4d7eb3fcb74064cfd78746d79633f5b895d341b1
Niccolo' Campolungo <damnednickix@hotmail.it>
2010-08-14 15:25:53 +0200

    Piccolo refactoring

--- src/index.py
+++ src/index.py
@@ -13,7 +13,7 @@ def index():
 def topic(id):
     topic_dict = db.topics.find_one({'id': id})
     topic_title = "Errore"
-    if topic_dict:
+    try:
         topic_title = topic_dict['title']
         topic_author_id = topic_dict['author']
         topic_author_dict = db.users.find_one({'id': topic_author_id})
@@ -22,9 +22,9 @@ def topic(id):
         topic_id = topic_dict['id']
         topic_desc = topic_dict['desc']
         topic_content = topic_dict['content']
-        return render_template('topic.html', **locals())
-    else:
-        return render_template('topic.html', **locals())
+    except:
+        pass
+    return render_template('topic.html', **locals())
 
 @app.route("/section/<id>")
 def section(id):
@@ -34,16 +34,16 @@ def section(id):
 def user(id):
     user_dict = db.users.find_one({'id': id})
     user_title = "Errore"
-    if user_dict:
+    try:
         user_nick = user_dict['nick']
         user_title = "Profilo di {nick}".format(nick=user_nick)
         user_id = id
         user_email = user_dict['email']
         user_admin = user_dict['admin']
         user_admin_str = user_admin and "L'utente {nick} e' un admin del sito".format(nick=user_nick) or ''
-        return render_template('user.html', **locals())
-    else:
-        return render_template('user.html', **locals())
+    except:
+        pass
+    return render_template('user.html', **locals())
 
 if __name__ == '__main__':
     app.debug = True
